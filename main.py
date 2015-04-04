59c47cab815ff93dd66adcc0c7b5f9d05024f429
Jack Singleton <git@jacksingleton.com>
2015-04-04 10:38:46 -0700

    extract route patch into Flask subclass

--- main.py
+++ main.py
@@ -3,41 +3,40 @@
 from flask import Flask, request, redirect, url_for, make_response
 import functools
 
-app = Flask(__name__)
+class CustomFlask(Flask):
+
+    def _logged_in(self):
+        return request.args.get('auth') is not None
+
+    def _login_required(self, func):
+        @functools.wraps(func)
+        def wrapper(*args, **kwargs):
+            if not self._logged_in():
+                return make_response('Unauthorized', 401)
+            return func(*args, **kwargs)
+        return wrapper
+
+    def _with_auth(self, viewfunc, public):
+        if public is True:
+            return viewfunc
+        else:
+            return self._login_required(viewfunc)
+
+    def route(self, rule, **options):
+        def decorator(f):
+            is_public = options.pop('public', False)
+            protected_f = self._with_auth(f, is_public)
+            return Flask.route(self, rule, **options)(protected_f)
+        return decorator
+
+app = CustomFlask(__name__)
 
 
-def logged_in():
-    return request.args.get('auth') is not None
-
-def login_required(func):
-    @functools.wraps(func)
-    def wrapper(*args, **kwargs):
-        if not logged_in():
-            return make_response('Unauthorized', 401)
-        return func(*args, **kwargs)
-    return wrapper
-
-def with_auth(viewfunc, public):
-    if public is True:
-        return viewfunc
-    else:
-        return login_required(viewfunc)
-
-def custom_route(rule, **options):
-    def decorator(f):
-        is_public = options.pop('public', False)
-        protected_f = with_auth(f, is_public)
-        endpoint = options.pop('endpoint', None)
-        app.add_url_rule(rule, endpoint, protected_f, **options)
-        return protected_f
-    return decorator
-
-
-@custom_route('/open', public=True)
+@app.route('/open', public=True)
 def hello_world():
     return 'Hello World!'
 
-@custom_route('/closed')
+@app.route('/closed')
 def goodbye_world():
     return "You're in! Good job"
 
